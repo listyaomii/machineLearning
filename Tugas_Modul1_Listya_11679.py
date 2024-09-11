29d52318b2381539d565f1cf207c3a94dce7664e
listyaomii <220711679@students.uajy.ac.id>
2024-09-11 16:12:21 +0700

    Update Tugas_Modul1_Listya_11679.py

--- Tugas_Modul1_Listya_11679.py
+++ Tugas_Modul1_Listya_11679.py
@@ -4,10 +4,10 @@ import pickle
 import os
  
 #load model
-model_directory = r'D:\coolyeah\semester5\Tugas_Modul1_Listya_11679' ##diisi dengan path folder dimana file model berada
+ ##diisi dengan path folder dimana file model berada
  
 # Gunakan os.path.join() untuk menggabungkan direktori dan file model pickle
-model_path = os.path.join(model_directory, 'rf_diabetes_model.pkl') 
+model_path = 'rf_diabetes_model.pkl'
  
 # Periksa apakah file ada di direktori yang ditentukan
 if os.path.exists(model_path):
